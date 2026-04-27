b327fe2d3120d96d95ac816dd8ff17727865321a
JunggleVincent81 <tegarbagussantoso1116@gmail.com>
2026-04-27 10:47:07 +0700

    setup backend expressjs+setup database mysql

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Vendora API Running");
+});
+require("./config/db");
+module.exports = app;
